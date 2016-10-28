92f48cb7358fcf5b1eb2e0c51ea6b95c0899a5e7
sidediallo <sidediallo@yahoo.fr>
2016-10-28 15:01:32 +0200

    Ajout d'un fichier de test PersonManagerTest.

---
+++ src/test/java/fr/univ_amu/yearbook/dao/PersonManagerTest.java
@@ -0,0 +1,59 @@
+package fr.univ_amu.yearbook.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PersonManagerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testFindPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllPersons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveOrUpdatePerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemovePersonLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemovePersonPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAllPersons() {
+		fail("Not yet implemented");
+	}
+
+}
