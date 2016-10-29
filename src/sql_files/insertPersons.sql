989096f60adbbe48638e12e68dd6f544a148842e
sidediallo <sidediallo@yahoo.fr>
2016-10-30 00:06:51 +0200

    Insertion de données de test dans la table JEE_Person

---
+++ src/sql_files/insertPersons.sql
@@ -0,0 +1,3 @@
+INSERT INTO JEE_Person (lastName, firstName, email, homepage, birthDate, passWordG, idG) VALUES ('DIALLO', 'Aboubacar Sidy', 'sidediallo@yahoo.fr', 'www.diallo_aboubacar.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('aboubacar'), 1);
+INSERT INTO JEE_Person (lastName, firstName, email, homepage, birthDate, passWordG, idG) VALUES ('KONDI', 'Marie', 'kondimarie@gmail.com', 'www.kondi_marie.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('marie'), 1);
+INSERT INTO JEE_Person (lastName, firstName, email, homepage, birthDate, passWordG, idG) VALUES ('CAMARA', 'Moussa', 'camaramoussa@gmail.com', 'www.camara_moussa.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('moussa'), 1);
