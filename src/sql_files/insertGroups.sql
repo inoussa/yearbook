b24a35545d19faed95077dad82071b37a67f2f81
sidediallo <sidediallo@yahoo.fr>
2016-10-29 13:16:46 +0200

    Insertion des groupes dans la table JEE_Group

---
+++ src/sql_files/insertGroups.sql
@@ -0,0 +1,11 @@
+-- Master 2 informatique Aix-Marseille Université
+
+INSERT INTO JEE_Group (groupName) VALUES ('M2_FED');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_RO');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_IF');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_RIM');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_SIS');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_ISL');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_FSI');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_GL');
+INSERT INTO JEE_Group (groupName) VALUES ('M2_SIR');
