044e15e31bdf34ad603bd40e1ba41b9deb192177
sidediallo <sidediallo@yahoo.fr>
2016-10-31 10:45:30 +0100

    Insertion des données de test

--- src/sql_files/insertGroups.sql
+++ src/sql_files/insertGroups.sql
@@ -1,11 +1,11 @@
--- Master 2 informatique Aix-Marseille Université
+-- Données de test insertion de groupes dans la base
 
-INSERT INTO JEE_Group (groupName) VALUES ('M2_FED');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_RO');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_IF');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_RIM');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_SIS');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_ISL');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_FSI');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_GL');
-INSERT INTO JEE_Group (groupName) VALUES ('M2_SIR');
+INSERT INTO JEE_Group (name) VALUES ('M2_FED');
+INSERT INTO JEE_Group (name) VALUES ('M2_RO');
+INSERT INTO JEE_Group (name) VALUES ('M2_IF');
+INSERT INTO JEE_Group (name) VALUES ('M2_RIM');
+INSERT INTO JEE_Group (name) VALUES ('M2_SIS');
+INSERT INTO JEE_Group (name) VALUES ('M2_ISL');
+INSERT INTO JEE_Group (name) VALUES ('M2_FSI');
+INSERT INTO JEE_Group (name) VALUES ('M2_GL');
+INSERT INTO JEE_Group (name) VALUES ('M2_SIR');
