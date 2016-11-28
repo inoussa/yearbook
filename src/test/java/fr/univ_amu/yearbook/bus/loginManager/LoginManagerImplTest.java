a44b268388814420c1e406fac051b875cc4c25a7
sidediallo <sidediallo@yahoo.fr>
2016-11-28 22:52:48 +0100

    package de test loginManager

---
+++ src/test/java/fr/univ_amu/yearbook/bus/loginManager/LoginManagerImplTest.java
@@ -0,0 +1,60 @@
+package fr.univ_amu.yearbook.bus.loginManager;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import fr.univ_amu.yearbook.bean.Person;
+import fr.univ_amu.yearbook.bus.exception.ManagerException;
+import fr.univ_amu.yearbook.dao.IPersonDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring_test.xml"})
+public class LoginManagerImplTest {
+
+	@Autowired
+	ILoginManager loginManager;
+	
+	@Autowired
+	IPersonDAO personDao;
+	
+	@Test
+	public void testPersonAssociedLoginPwd() throws ManagerException {
+		Person p = new Person();
+		
+		p.setLastName("JOBS");
+		p.setFirstName("Steve");
+		p.setEmail("steve.jobs@localhost.fr");
+		p.setHomePage("www.steve_jobs.fr");
+		p.setBirthDate(Date.valueOf("2016-01-01"));
+		p.setPwd("steve");
+		p.setIdG((long) 2);
+		personDao.saveOrUpdatePerson(p);
+		
+		String email = "steve.jobs@localhost.fr";
+		String pwd = "steve";
+		
+		Person p1 = loginManager.personAssociedLoginPwd(email, pwd);
+		
+		assertNotNull(p1);
+		assertTrue("true", p1 instanceof Person);
+		
+		p1 = loginManager.personAssociedLoginPwd(email, "ceci n'est pas un mdp valide");
+		assertNull(p1);
+		assertFalse(p1 instanceof Person);
+		
+		p1 = loginManager.personAssociedLoginPwd("pas.valide@localhost.fr", "ceci n'est pas un mdp valide");
+		assertNull(p1);
+		assertFalse(p1 instanceof Person);
+		
+		personDao.removePerson(p);
+	}
+
+}
