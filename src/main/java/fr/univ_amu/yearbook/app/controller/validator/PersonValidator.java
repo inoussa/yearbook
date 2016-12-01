21fb610164203056203e0b597178c9680929b297
sidediallo <sidediallo@yahoo.fr>
2016-12-01 05:42:52 +0100

    Vérif de la validité des données lors de l'inscription d'une personne.

--- src/main/java/fr/univ_amu/yearbook/app/controller/validator/PersonValidator.java
+++ src/main/java/fr/univ_amu/yearbook/app/controller/validator/PersonValidator.java
@@ -2,19 +2,37 @@ package fr.univ_amu.yearbook.app.controller.validator;
 
 import java.util.Calendar;
 
+import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import fr.univ_amu.yearbook.bean.Person;
 
+@Service("personValidator")
 public class PersonValidator implements Validator {
+	
+	/**
+	 * 
+	 */
+	private final String EMAIL_PATTERN = "^[a-z][\\.\\w]*@[a-z]+[a-z\\.-]+[a-z]+\\.[a-z]{2,4}$";
+	
+	/**
+	 * 
+	 */
+	private final String PWD_PATTERN = "[a-z1-9]+";
 
+	/**
+	 * 
+	 */
     @Override
     public boolean supports(Class<?> clazz) {
         return Person.class.isAssignableFrom(clazz);
     }
 
+    /**
+     * 
+     */
     @Override
     public void validate(Object target, Errors errors) {
         Person person = (Person) target;
@@ -25,8 +43,11 @@ public class PersonValidator implements Validator {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName",
                 "person.firstName");
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email",
-                "person.email");
+        if (person.getEmail() != null) {
+        	if (!person.getEmail().matches(EMAIL_PATTERN)) {
+                errors.rejectValue("email", "person.email");
+            }
+        }
         
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "homePage",
                 "person.homePage");
@@ -38,12 +59,9 @@ public class PersonValidator implements Validator {
         }
         
         if (person.getPwd() != null) {
-            if (!person.getPwd().matches("[A-Z]")) {
+            if (!person.getPwd().matches(PWD_PATTERN)) {
                 errors.rejectValue("pwd", "person.pwd");
             }
         }
-        
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "idG",
-                "person.idG");
     }
 }
