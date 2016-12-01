5120dd52fada3a9fd44506e4900d25bae0dd4261
sidediallo <sidediallo@yahoo.fr>
2016-12-01 13:53:05 +0100

    Ajout des commentaires javadoc et gestion du required=false pour l'attribut homePage..

--- src/main/java/fr/univ_amu/yearbook/app/controller/validator/PersonValidator.java
+++ src/main/java/fr/univ_amu/yearbook/app/controller/validator/PersonValidator.java
@@ -9,21 +9,41 @@ import org.springframework.validation.Validator;
 
 import fr.univ_amu.yearbook.bean.Person;
 
+/**
+ * <b>PersonValidator<b/> gère la validation des données rentrées par
+ * un utilisateur lors de son inscription.
+ * 
+ * Cette classe comprend :
+ * <ul>
+ * <li>Une expression régulière sur l'email.</li>
+ * <li>Une expression régulière sur le mot de passe.</li>
+ * </ul>
+ * 
+ * @author Aboubacar Sidy DIALLO
+ * @author Inoussa ZONGO
+ *
+ * @version 1.2
+ */
 @Service("personValidator")
 public class PersonValidator implements Validator {
 	
 	/**
+	 * Le format du mail doit appartenir au parttern.
 	 * 
+	 * @see #validate(Object, Errors)
 	 */
 	private final String EMAIL_PATTERN = "^[a-z][\\.\\w]*@[a-z]+[a-z\\.-]+[a-z]+\\.[a-z]{2,4}$";
 	
 	/**
+	 * Le format du mot de passe doit appartenir au parttern.
 	 * 
+	 * @see #validate(Object, Errors)
 	 */
 	private final String PWD_PATTERN = "[a-z1-9]+";
 
 	/**
-	 * 
+	 * @param clazz Le type entré en paramètre.
+	 * @return True si le type correspond au type Person et false sinon.
 	 */
     @Override
     public boolean supports(Class<?> clazz) {
@@ -31,7 +51,13 @@ public class PersonValidator implements Validator {
     }
 
     /**
+     * Gère la validité des données de l'utilisateur.
      * 
+     * @param target L'objet sur lequel on ferra le test.
+     * @param errors La classe errors.
+     * 
+     * @see Object
+     * @see Error
      */
     @Override
     public void validate(Object target, Errors errors) {
@@ -48,9 +74,6 @@ public class PersonValidator implements Validator {
                 errors.rejectValue("email", "person.email");
             }
         }
-        
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "homePage",
-                "person.homePage");
         
         if (person.getBirthDate() != null) {
         	if (person.getBirthDate().after(Calendar.getInstance().getTime())) {
