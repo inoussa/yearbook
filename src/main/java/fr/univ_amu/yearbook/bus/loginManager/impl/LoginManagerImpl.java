1111e5ce315101928f651024521e340b2da631f6
sidediallo <sidediallo@yahoo.fr>
2016-12-05 20:45:26 +0100

    Modif de l'implantation de la classe (Prise en compte de la DAO)

--- src/main/java/fr/univ_amu/yearbook/bus/loginManager/impl/LoginManagerImpl.java
+++ src/main/java/fr/univ_amu/yearbook/bus/loginManager/impl/LoginManagerImpl.java
@@ -1,21 +1,13 @@
 package fr.univ_amu.yearbook.bus.loginManager.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.univ_amu.yearbook.bean.Person;
 import fr.univ_amu.yearbook.bus.exception.ManagerException;
 import fr.univ_amu.yearbook.bus.loginManager.ILoginManager;
-import fr.univ_amu.yearbook.dao.IDatabaseManager;
-import fr.univ_amu.yearbook.dao.IResultSetToBean;
+import fr.univ_amu.yearbook.dao.IPersonDAO;
 import fr.univ_amu.yearbook.dao.exception.DAOException;
-import fr.univ_amu.yearbook.dao.exception.DatabaseManagerException;
-import fr.univ_amu.yearbook.dao.impl.ResultSetToBeanImpl;
 
 /**
  * <b>LoginManagerImpl</b> est la classe qui implemente l'interface
@@ -23,12 +15,12 @@ import fr.univ_amu.yearbook.dao.impl.ResultSetToBeanImpl;
  * 
  * Cette classe est caractérisée par :
  * <ul>
- * <li>Un objet qui crée une connection à la base de données.</li>
+ * <li>Un objet de type IPersonDAO.</li>
  * </ul>
  * 
  * @see Person
  * @see ManagerException
- * @see IDatabaseManager
+ * @see IPersonManager
  * 
  * @author Aboubacar Sidy DIALLO
  * @author Inoussa ZONGO
@@ -39,12 +31,12 @@ import fr.univ_amu.yearbook.dao.impl.ResultSetToBeanImpl;
 public class LoginManagerImpl implements ILoginManager {
 	
 	/**
-	 * L'objet qui établi la connection avec la base.
+	 * L'accès aux méthodes de la DAO.
 	 * 
 	 * @see #personAssociedLoginPwd(String, String)
 	 */
 	@Autowired
-	private IDatabaseManager dbManager;
+	private IPersonDAO personDAO;
 
 	/**
 	 * Renvoie la personne associée à l'email et au mot de passe.
@@ -57,24 +49,10 @@ public class LoginManagerImpl implements ILoginManager {
 	@Override
 	public Person personAssociedLoginPwd(String email, String pwd) throws ManagerException {
 		
-		try (Connection conn = dbManager.newConnection()) {
-			IResultSetToBean<Person> mapper = new ResultSetToBeanImpl<Person>(Person.class);
-			
-			String query = "SELECT * "
-					+ "FROM YEARBOOK_Person "
-					+ "WHERE email = ? AND pwd = PASSWORD(?)";			
-			PreparedStatement st = conn.prepareStatement(query);
-			
-			st.setString(1, email);
-			st.setString(2, pwd);
-			
-			ResultSet rs = st.executeQuery();
-			
-			if (rs.next())
-				return mapper.toBean(rs);
-			return null;
-		} catch (SQLException | DatabaseManagerException | DAOException e){
-			throw new ManagerException(e.getCause());
-		}
+			try {
+				return personDAO.personAssociedLoginPwd(email, pwd);
+			} catch (DAOException e) {
+				throw new ManagerException(e.getCause());
+			}
 	}
 }
