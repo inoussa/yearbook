dac40a867a5f78b99e754b2321631ccd1b954d71
sidediallo <sidediallo@yahoo.fr>
2016-12-13 16:13:16 +0100

    Ajout de scope session

--- src/main/java/fr/univ_amu/yearbook/bean/Person.java
+++ src/main/java/fr/univ_amu/yearbook/bean/Person.java
@@ -1,12 +1,17 @@
 package fr.univ_amu.yearbook.bean;
 
+import java.io.Serializable;
 import java.sql.Date;
-
+/*
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
+*/
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
 /**
  * <b>Person</b> est la classe qui permet de créer une personne
@@ -30,8 +35,12 @@ import javax.validation.constraints.Pattern;
  * @version 1.2
  *
  */
-public class Person {
+@Component()
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Person implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * L'identifiant d'une personne. Cet id n'est pas modifiable.
 	 * 
@@ -46,10 +55,12 @@ public class Person {
 	 * @see #getLastName()
 	 * @see #setLastName(String)
 	 */
+	/*
 	@NotNull
 	@Min(value=2, message="Le nom doit contenir au moins 2 caractères")
     @Max(value=20, message="Le nom doit contenir au plus 20 caractères")
-	@Pattern(regexp="[A-Z][A-Za-Z-]*", message="Le nom doit débuter par une majuscule et contenir que des lettres ou -")
+	@Pattern(regexp="[A-Z][a-zA-Z]+", message="Le nom doit débuter par une majuscule et contenir que des lettres ou -")
+	*/
 	private String lastName;
 	
 	/**
@@ -58,10 +69,12 @@ public class Person {
 	 * @see #getFirstName()
 	 * @see #setFirstName(String)
 	 */
+	/*
 	@NotNull
 	@Min(value=2, message="Le prénom doit contenir au moins 2 caractères")
     @Max(value=20, message="Le prénom doit contenir au plus 20 caractères")
-	@Pattern(regexp="[A-Z][A-Za-Z-]*", message="Le prénom doit débuter par une majuscule et contenir que des lettres ou -")
+	@Pattern(regexp="[A-Z][A-Za-z-]+", message="Le prénom doit débuter par une majuscule et contenir que des lettres ou -")
+	*/
 	private String firstName;
 	
 	/**
@@ -70,10 +83,12 @@ public class Person {
 	 * @see #getEmail()
 	 * @see #setEmail(String)
 	 */
+	/*
 	@NotNull
 	@Min(value=8, message="L'email doit contenir au moins 8 caractères")
     @Max(value=20, message="L'email doit contenir au plus 20 caractères")
-	@Pattern(regexp="^[a-z][\\.\\w]*@[a-z]+[a-z\\.-]+[a-z]+\\.[a-z]{2,4}$", message="Email valide : test@etu.univ-amu.fr")
+	@Pattern(regexp="^[a-z][\\.\\w]*@[a-z]+[a-z\\.-]+[a-z]+\\.[a-z]{2,4}", message="Email valide : test@etu.univ-amu.fr")
+	*/
 	private String email;
 	
 	/**
@@ -82,10 +97,12 @@ public class Person {
 	 * @see #getHomePage()
 	 * @see #setHomePage(String)
 	 */
+	/*
 	@Null
 	@Min(value=8, message="La page web doit contenir au moins 8 caractères")
     @Max(value=20, message="La page web doit contenir au plus 20 caractères")
-	@Pattern(regexp="[w]{3}\\.[a-z\\._-]+\\.[a-z\\.~/_-]{2,}$", message="Exemple de page web valide : www.etu.univ-amu.fr")
+	@Pattern(regexp="[w]{3}\\.[a-z\\._-]+\\.[a-z\\.~/_-]{2,}", message="Exemple de page web valide : www.etu.univ-amu.fr")
+	*/
 	private String homePage;
 	
 	/**
