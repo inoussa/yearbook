783f7a77bdead452f1ea2817a3e1461d47453abf
sidediallo <sidediallo@yahoo.fr>
2016-12-19 21:29:29 +0100

    Prise en compte des erreurs de saisie

--- src/main/java/fr/univ_amu/yearbook/app/controller/validator/PersonValidator.java
+++ src/main/java/fr/univ_amu/yearbook/app/controller/validator/PersonValidator.java
@@ -32,6 +32,27 @@ public class PersonValidator implements Validator {
 	private final String HOMEPAGE_PATTERN = "[w]{3}\\.[a-z\\._-]+\\.[a-z\\.~/_-]{2,}$";
 	
 	/**
+	 * Le format du nom doit appartenir au parttern.
+	 * 
+	 * @see #validate(Object, Errors)
+	 */
+	private final String LASTNAME_PATTERN = "[A-Za-z\\s]{2,}$";
+	
+	/**
+	 * Le format du prénom doit appartenir au parttern.
+	 * 
+	 * @see #validate(Object, Errors)
+	 */
+	private final String FIRSTNAME_PATTERN = "[A-Za-z-\\s]{2,}$";
+	
+	/**
+	 * Le format du mail doit appartenir au parttern.
+	 * 
+	 * @see #validate(Object, Errors)
+	 */
+	private final String EMAIL_PATTERN = "^[a-z][\\.\\w]*@[a-z]+[a-z\\.-]+[a-z]+\\.[a-z]{2,4}$";
+	
+	/**
 	 * @param clazz Le type entré en paramètre.
 	 * @return True si le type correspond au type Person et false sinon.
 	 */
@@ -53,20 +74,34 @@ public class PersonValidator implements Validator {
     public void validate(Object target, Errors errors) {
         Person person = (Person) target;
         
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "person.lastName");
-
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "person.firstName");
-
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "person.email");
+        if (person.getLastName() != null) {
+        	if (!person.getLastName().matches(LASTNAME_PATTERN) || person.getLastName().length() > 20) {
+                errors.rejectValue("lastName", "person.lastName");
+            }
+        }
+        
+        if (person.getFirstName() != null) {
+        	if (!person.getFirstName().matches(FIRSTNAME_PATTERN) || person.getFirstName().length() > 20) {
+                errors.rejectValue("firstName", "person.firstName");
+            }
+        }
+        
+        if (person.getEmail() != null) {
+        	if (!person.getEmail().matches(EMAIL_PATTERN) || person.getEmail().length() > 30) {
+                errors.rejectValue("email", "person.email");
+            }
+        }
         
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "birthDate", "person.birthDate");
         
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "pwd", "person.pwd");
+        if (person.getPwd() != null && person.getPwd().length() < 6) {
+        	errors.rejectValue("pwd", "person.pwd");
+    	}
         
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "idG", "person.idG");
         
         if (person.getHomePage() != null) {
-            if (!person.getHomePage().matches(HOMEPAGE_PATTERN)) {
+            if (!person.getHomePage().matches(HOMEPAGE_PATTERN) || person.getHomePage().length() > 30) {
                 errors.rejectValue("homePage", "person.homePage");
             }
         }
